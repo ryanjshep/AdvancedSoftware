2c49ad993dbd5145e31dca96a2957e76f789ea81
bahamutr13 <tedlpascua@gmail.com>
2016-10-04 21:02:11 -0700

    template for adding to list

--- page/javascript/test.js
+++ page/javascript/test.js
@@ -1,5 +1,62 @@
-var takeTime;
+(function(){
 
-function testPilot(){
+var app = angular.module('store', [ ]);
 
+app.controller('StoreController', function($scope, $http){
+  $scope.carList = tmpCarList;
+
+  $scope.loadList = [];
+  console.log($scope.carList);
+
+  $scope.listToLoad = function(x){
+    console.log("I am Loading", x);
+    $scope.loadList.push(x);
+    $scope.findAndDelete($scope.carList, x.id);
+  }
+
+  $scope.loadToList = function(x){
+    console.log("I am removing car ", x, " from load");
+    $scope.carList.push(x);
+    $scope.findAndDelete($scope.loadList, x.id);
+  }
+
+  $scope.findAndDelete = function(array, index){
+    for(var i = 0; i < array.length; i++){
+      if(array[i].id == index){ 
+        array.splice(i, 1);
+        return;
+      }
+    }
+  }
+
+});
+
+var gem = {
+  name: 'Dodechaechedron',
+  price: 2.95,
+  description: ' . . . ',
+  loaded: false
 }
+
+var tmpCarList = [
+{
+  name: "Blue Car", 
+  id: 1,
+  date: "9/9/99"
+},
+{
+  name: "Red Car",
+  id: 2,
+  date: "5/9/99"
+},
+{
+  name: "Orange Car",
+  id: 3,
+  date: "1/9/99"
+}
+];
+
+
+})();
+
+
